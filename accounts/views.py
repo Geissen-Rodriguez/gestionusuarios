af4accbcbd9316258173b2f1bc667d0e1d2bc104
Geissen-Rodriguez <geissen.rodriguez@gmail.com>
2025-10-22 13:46:36 -0300

    cambio

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import DetailView, FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
